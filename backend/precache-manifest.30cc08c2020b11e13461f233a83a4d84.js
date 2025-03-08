a00223299e69a94bee7ccba2df26b259b44c5537
ymu4 <68370573+ymu4@users.noreply.github.com>
2025-03-08 14:28:53 +0400

    Add files via upload

---
+++ backend/precache-manifest.30cc08c2020b11e13461f233a83a4d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fe9365d25b8920a9c1652cfd8d1d47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8b54fc44079aed15fee",
+    "url": "/static/css/main.0b7bce46.chunk.css"
+  },
+  {
+    "revision": "46760b06868661d981ad",
+    "url": "/static/js/2.80c497f2.chunk.js"
+  },
+  {
+    "revision": "9019dde1bc22665fe6f0005f90209b76",
+    "url": "/static/js/2.80c497f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b54fc44079aed15fee",
+    "url": "/static/js/main.f3069f5c.chunk.js"
+  },
+  {
+    "revision": "b8fdabcc0c9f899011df",
+    "url": "/static/js/runtime-main.38647ceb.js"
+  }
+]);
